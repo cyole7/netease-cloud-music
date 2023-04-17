8f1e7eaba4609b0f5f341a2df91b046e80a9a42f
liyujie <hi@cyoo.cn>
2023-04-17 14:07:40 +0800

    chore: init

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
-function login() {
-  isLoginDialogOpen.value = true
-}
 </script>
 
 <template>
   <div>
     首页
-    <NButton @click="login">
+    <NButton @click="openLoginDialog()">
       登录
     </NButton>
-    <NButton @click="logout">
+    <NButton @click="logout()">
       退出
     </NButton>
   </div>
